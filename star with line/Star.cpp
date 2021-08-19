14e2a38a4d59ba11e1aa4d650fe5afdbf7004bd2
Azran Hossain <mr69751@gmail.com>
2021-08-20 00:59:49 +0600

    Create Star.cpp

---
+++ star with line/Star.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include<GL/gl.h>
+#include <GL/glut.h>
+
+void myDisplay(void)
+{
+glClear (GL_COLOR_BUFFER_BIT);
+glColor3ub (128, 128, 128);
+glPointSize(5.0);
+
+glBegin(GL_LINE_LOOP);
+glVertex2i(150, 240);
+glVertex2i(350, 100);
+glVertex2i(280, 290);
+glVertex2i(150, 100);
+glVertex2i(400, 240);
+
+
+
+glEnd();
+glFlush ();
+}
+
+void myInit (void)
+{
+glClearColor(0.0, 0.0, 0.0, 0.0);
+glMatrixMode(GL_PROJECTION);
+glLoadIdentity();
+gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+}
+
+
+
+
+
+int main(int argc, char** argv)
+{
+glutInit(&argc, argv);
+glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+glutInitWindowSize (640, 480);
+glutInitWindowPosition (100, 150);
+glutCreateWindow ("");
+glutDisplayFunc(myDisplay);
+myInit ();
+glutMainLoop();
+}
